b7ee443c0859b6db80341892a8b69616a9b57f48
Your Name <relaxjavaid@gmail.com>
2024-09-28 21:55:13 +0530

    added production url

--- utils/index.ts
+++ utils/index.ts
@@ -1,7 +1,7 @@
 export const getCanonicalUrl = () => {
     return process.env.NODE_ENV !== 'production'
         ? 'http://localhost:3000'
-        : 'https://nextjs-crash-course-theta.vercel.app';
+        : 'https://easy-sell-nextjs.vercel.app/';
 };
 
 export const getImageUrl = (imageUrl: string) => {
